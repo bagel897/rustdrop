001bfaa6c64a827005a0d1f7e3b9ac5d5cc84982
bagel897 <bageljr897@protonmail.com>
2023-05-07 15:51:06 -0500

    WIP

--- src/core/ukey2.rs
+++ src/core/ukey2.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 
-use prost::bytes::BytesMut;
+use prost::bytes::{BufMut, BytesMut};
+use prost::Message;
 use ring::hkdf::{KeyType, Salt, HKDF_SHA256};
 use ring::hmac::{self, Key};
 use ring::rand::SystemRandom;
@@ -10,6 +11,7 @@ const D2D_SALT_RAW: &'static str =
 const PT2_SALT_RAW: &'static str =
     "BF9D2A53C63616D75DB0A7165B91C1EF73E537F2427405FA23610A4BE657642E";
 use crate::protobuf::securegcm::Ukey2ClientFinished;
+use crate::wlan::init;
 
 pub fn get_public_private() -> Result<Ed25519KeyPair, Box<dyn Error>> {
     let rng = SystemRandom::new();
@@ -45,11 +47,41 @@ pub(crate) struct Ukey2 {
     encrypt_key: Key,
     send_hmac: Key,
 }
+fn diffie_hellmen(
+    client_pub: <Ed25519KeyPair as KeyPair>::PublicKey,
+    server_key: Ed25519KeyPair,
+) -> BytesMut {
+    let dhs = todo!();
+    dhs
+}
 fn key_echange(
     client_pub: <Ed25519KeyPair as KeyPair>::PublicKey,
     server_key: Ed25519KeyPair,
-) -> (Vec<u8>, Vec<u8>) {
-    todo!();
+    init: BytesMut,
+    resp: BytesMut,
+) -> (BytesMut, BytesMut) {
+    let dhs = diffie_hellmen(client_pub, server_key);
+    let mut xor = BytesMut::with_capacity(usize::max(init.len(), resp.len()));
+    let default: u8 = 0x0;
+    for i in 0..xor.capacity() {
+        xor.put_bytes(
+            init.get(i).unwrap_or(&default) ^ resp.get(i).unwrap_or(&default),
+            1,
+        )
+    }
+    let xor_buf = [xor.as_ref()];
+    let l_auth = HKDF_SHA256;
+    let l_next = HKDF_SHA256;
+    let prk_auth = Salt::new(HKDF_SHA256, "UKEY2 v1 auth".as_bytes()).extract(&dhs);
+    let prk_next = Salt::new(HKDF_SHA256, "UKEY2 v1 next".as_bytes()).extract(&dhs);
+    let auth_string = prk_auth.expand(&xor_buf, l_auth).unwrap();
+    let next_secret = prk_next.expand(&xor_buf, l_next).unwrap();
+    let mut auth_buf = BytesMut::with_capacity(auth_string.len().len());
+    auth_string.fill(&mut auth_buf).unwrap();
+    let mut next_buf = BytesMut::with_capacity(next_secret.len().len());
+    next_secret.fill(&mut next_buf).unwrap();
+
+    return (auth_buf, next_buf);
 }
 impl Ukey2 {
     pub fn new(
@@ -61,9 +93,11 @@ impl Ukey2 {
         let D2D_SALT: Salt = Salt::new(HKDF_SHA256, D2D_SALT_RAW.as_bytes());
         let PT2_SALT: Salt = Salt::new(HKDF_SHA256, PT2_SALT_RAW.as_bytes());
         let client_pub_key = get_public(client_resp.public_key());
-        let (auth_string, next_protocol_secret) = key_echange(client_pub_key, server_key_pair);
-        let d2d_client = get_hdkf_key_raw("client", next_protocol_secret.as_slice(), &D2D_SALT);
-        let d2d_server = get_hdkf_key_raw("server", next_protocol_secret.as_slice(), &D2D_SALT);
+        let resp_buf = BytesMut::from(client_resp.encode_to_vec().as_slice());
+        let (auth_string, next_protocol_secret) =
+            key_echange(client_pub_key, server_key_pair, init, resp_buf);
+        let d2d_client = get_hdkf_key_raw("client", &next_protocol_secret, &D2D_SALT);
+        let d2d_server = get_hdkf_key_raw("server", &next_protocol_secret, &D2D_SALT);
         let decrypt_key = get_hdkf_key("ENC:2", &d2d_client, &PT2_SALT);
         let recieve_key = get_hdkf_key("SIG_1", &d2d_client, &PT2_SALT);
         let encrypt_key = get_hdkf_key("ENC:2", &d2d_server, &PT2_SALT);
@@ -91,6 +125,6 @@ mod tests {
     fn test_key_exchange() {
         let server_keypair = get_public_private().unwrap();
         let client_keypair = get_public_private().unwrap();
-        key_echange(*client_keypair.public_key(), server_keypair);
+        diffie_hellmen(*client_keypair.public_key(), server_keypair);
     }
 }
