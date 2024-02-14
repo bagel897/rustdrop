56a4fa6952d4012dc08602f83854f81ee2b2d2cf
bagel897 <ellenagarwal897@gmail.com>
2024-02-13 19:59:40 -0600

    Update gtk4 to use new API

--- rustdrop_gtk4/src/daemon.rs
+++ rustdrop_gtk4/src/daemon.rs
@@ -1,64 +1,12 @@
-use crate::event_loop::runtime;
-use async_trait::async_trait;
-use flume::{Receiver, RecvError, Sender};
-use glib::clone;
-use rustdrop::{Config, Device, IncomingText, Rustdrop, UiHandle};
-use tokio_util::sync::CancellationToken;
+use flume::{Receiver, Sender};
+use rustdrop::{Config, Device, DiscoveryEvent, Rustdrop};
 
-#[derive(Debug)]
-struct ChanUiHandle {
-    recv: Receiver<Device>,
-    send: Sender<Device>,
-}
-#[derive(Debug, Clone)]
-pub(crate) struct Handler {
-    recv: Receiver<Device>,
-    send: Sender<Device>,
-    cancel: CancellationToken,
-}
-async fn run_child(child: ChanUiHandle, token: CancellationToken) {
-    let mut rustdrop = Rustdrop::from_ui(child, Config::default()).await.unwrap();
-    rustdrop.send_file().await.unwrap();
-    token.cancelled().await;
+pub async fn run_child(rx: Receiver<Device>, send: Sender<Receiver<DiscoveryEvent>>) {
+    let mut rustdrop = Rustdrop::new(Config::default()).await.unwrap();
+    let discovery = rustdrop.discover().await.unwrap();
+    send.send_async(discovery).await.unwrap();
+    while let Ok(dev) = rx.recv_async().await {
+        rustdrop.send_file(dev).await.unwrap();
+    }
     eprintln!("Shutting down daemon");
-}
-impl Handler {
-    pub fn new() -> Self {
-        let (send, recv) = flume::unbounded();
-        let (send_confirm, recv_confirm) = flume::unbounded();
-        let cancel = CancellationToken::new();
-        let child = ChanUiHandle {
-            send,
-            recv: recv_confirm,
-        };
-        runtime().spawn(clone!(@strong cancel => async move { run_child(child, cancel).await }));
-        Self {
-            recv,
-            send: send_confirm,
-            cancel,
-        }
-    }
-    pub async fn get_device(&self) -> Result<Device, RecvError> {
-        self.recv.recv_async().await
-    }
-    pub fn pick_dest(&self, device: Device) {
-        self.send.send(device).unwrap()
-    }
-}
-#[async_trait]
-impl UiHandle for ChanUiHandle {
-    async fn discovered_device(&self, device: Device) {
-        self.send.send_async(device).await.unwrap()
-    }
-    async fn handle_text(&mut self, text: IncomingText) {
-        todo!()
-    }
-
-    async fn handle_pairing_request(&mut self, request: &rustdrop::PairingRequest) -> bool {
-        todo!()
-    }
-
-    async fn pick_dest(&self) -> Option<Device> {
-        self.recv.recv_async().await.ok()
-    }
 }
--- rustdrop_gtk4/src/main.rs
+++ rustdrop_gtk4/src/main.rs
@@ -7,15 +7,19 @@ use adw::prelude::*;
 
 use adw::{ActionRow, Application, ApplicationWindow, HeaderBar};
 use consts::ID;
-use daemon::Handler;
+use daemon::run_child;
+use event_loop::runtime;
 use gtk::glib::clone;
 use gtk::{Box, ListBox, Orientation, SelectionMode};
+use rustdrop::DiscoveryEvent;
 
 fn main() {
     tracing_subscriber::fmt::init();
     let application = Application::builder().application_id(ID).build();
     application.connect_activate(|app| {
-        let mut handler = Handler::new();
+        let (tx, rx) = flume::bounded(1);
+        let (tx_send, rx_send) = flume::unbounded();
+        runtime().spawn(async move { run_child(rx_send, tx).await });
         let list = ListBox::builder()
             .margin_top(32)
             .margin_end(32)
@@ -25,9 +29,10 @@ fn main() {
             // makes the list look nicer
             .css_classes(vec![String::from("boxed-list")])
             .build();
+        let discovery = rx.recv().unwrap();
         glib::spawn_future_local(clone!(@weak list => async move {
             let mut seen = HashSet::new();
-            while let Ok(dev) = handler.get_device().await {
+            while let Ok(DiscoveryEvent::Discovered(dev)) = discovery.recv_async().await {
                 if seen.contains(&dev) {
                     continue;
                 }
@@ -36,8 +41,8 @@ fn main() {
                     .activatable(true)
                     .title(format!("{}: {:?}",dev.device_name.clone(), dev.discovery))
                     .build();
-                row.connect_activated(clone!(@strong handler => move |_| {
-                    handler.pick_dest(dev.clone());
+                row.connect_activated( clone!(@strong tx_send => move |_| {
+                    tx_send.send(dev.clone()).unwrap();
                 }));
                 list.append(&row);
             }
