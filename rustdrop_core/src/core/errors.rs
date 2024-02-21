301df27e37565b42f8818673f6393b91cf19bd00
bagel897 <ellenagarwal897@gmail.com>
2024-02-21 15:51:49 -0600

    Transparent errors

--- rustdrop_core/src/core/errors.rs
+++ rustdrop_core/src/core/errors.rs
@@ -5,17 +5,8 @@ use tokio::io;
 use crate::protobuf::securegcm::Ukey2Alert;
 #[derive(Error, Debug)]
 pub enum RustdropError {
-    #[error("Openssl Error {0}")]
-    OpenSSL(#[from] ErrorStack),
     #[error("Encryption Error")]
     Encryption(),
-    #[error("Decode Error {source}")]
-    Decode {
-        #[from]
-        source: prost::DecodeError,
-    },
-    #[error("Base64 Decode Error")]
-    Base64(#[from] base64::DecodeError),
     #[error("Stream closed")]
     StreamClosed(),
     #[error("No Response")]
@@ -24,15 +15,24 @@ pub enum RustdropError {
     InvalidMessage(String),
     #[error("Invalid endpoint id")]
     InvalidEndpointId(),
-    #[error("Bluetooth Error {source}")]
-    Bluetooth {
-        #[from]
-        source: bluer::Error,
-    },
     #[error("Connection Error")]
     Connection(),
     #[error("Ukey Error {0:?}")]
     UkeyError(Ukey2Alert),
-    #[error("Io Error {0:?}")]
+    #[error(transparent)]
     Io(#[from] io::Error),
+    #[error(transparent)]
+    Base64(#[from] base64::DecodeError),
+    #[error(transparent)]
+    Bluetooth {
+        #[from]
+        source: bluer::Error,
+    },
+    #[error(transparent)]
+    Decode {
+        #[from]
+        source: prost::DecodeError,
+    },
+    #[error(transparent)]
+    OpenSSL(#[from] ErrorStack),
 }
